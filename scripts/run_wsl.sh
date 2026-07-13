76fda3a96a6fa6e39c255af1e167713c8a5f540e
ychen921 <steven88669@gmail.com>
2026-07-13 21:36:41 +0800

    fix(wsl): mount persistent writable container home

--- scripts/run_wsl.sh
+++ scripts/run_wsl.sh
@@ -8,6 +8,9 @@ HOST_UID="$(id -u)"
 HOST_GID="$(id -g)"
 HOST_USER="$(id -un)"
 HOST_HOSTNAME="$(hostname)"
+CONTAINER_HOME="$HOME/.docker-homes/${CONTAINER_NAME}"
+
+mkdir -p "${CONTAINER_HOME}"
 
 docker run -it --rm \
   --name "${CONTAINER_NAME}" \
@@ -19,6 +22,7 @@ docker run -it --rm \
   --ipc host \
   -e USER="${HOST_USER}" \
   -e LOGNAME="${HOST_USER}" \
+  -e HOME="/home/${HOST_USER}" \
   -e ROS_DOMAIN_ID=30 \
   -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
   -e ROS_LOCALHOST_ONLY=0 \
@@ -26,6 +30,7 @@ docker run -it --rm \
   -v "${WS}/bags:/bags" \
   -v "${WS}/data:/data" \
   -v "${WS}/results:/results" \
+  -v "${CONTAINER_HOME}:/home/${HOST_USER}" \
   -w /workspaces/rgbd-characterization \
   "${IMAGE_NAME}" \
   bash
