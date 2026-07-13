0252335d5f4b12ffca16203415f37996ab159ae0
ychen921 <steven88669@gmail.com>
2026-07-13 20:53:17 +0800

    chore: run WSL container with host user identity

--- scripts/run_wsl.sh
+++ scripts/run_wsl.sh
@@ -2,13 +2,23 @@
 set -e
 
 IMAGE_NAME="ros2-humble:latest"
-CONTAINER_NAME="ros2-humble-rgbd-wsl"
+CONTAINER_NAME="ros2-humble-orbbec-wsl"
 WS="$HOME/dev/rgbd-characterization"
+HOST_UID="$(id -u)"
+HOST_GID="$(id -g)"
+HOST_USER="$(id -un)"
+HOST_HOSTNAME="$(hostname)"
 
 docker run -it --rm \
   --name "${CONTAINER_NAME}" \
+  --hostname "${HOST_HOSTNAME}" \
+  --user "${HOST_UID}:${HOST_GID}" \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/group:/etc/group:ro \
   --network host \
   --ipc host \
+  -e USER="${HOST_USER}" \
+  -e LOGNAME="${HOST_USER}" \
   -e ROS_DOMAIN_ID=30 \
   -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
   -e ROS_LOCALHOST_ONLY=0 \
